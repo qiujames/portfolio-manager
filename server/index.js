1c5ca22c5746b002633bb75bad37d7fa6d30e21e
James Qiu <52264658+qiujames@users.noreply.github.com>
2024-03-07 22:24:53 -0800

    add server setup (#5)

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 5, // 5 requests max
+});
+app.use(limiter);
+app.set('trust proxy', 1);
+
+// Enable Cors
+app.use(cors());
+
+// Routes
+app.use('/api', require('./routes/stocks'));
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
