43b678731f38cb8b5738731b07967edf943b0624
James Qiu <qiujames@cs.washington.edu>
2024-03-08 19:45:54 -0800

    access all stock data

--- src/api/stock_api.jsx
+++ src/api/stock_api.jsx
@@ -1,7 +1,3 @@
-import config from '../config';
-import jsonData from '../sample_response';
-
-const API_KEY = config.apiKey;
 const METADATA_KEY = 'Meta Data';
 const LATEST_REFRESH_KEY = '3. Last Refreshed';
 const TIME_SERIES_KEY = 'Time Series (5min)';
@@ -9,34 +5,15 @@ const CLOSE_VALUE_KEY = '4. close';
 
 const fetchStockPrice = async (ticker) => {
   try {
-    // Make API request to get stock prices
-    let lastValue;
-    if (ticker === 'VOO') {
-      const stockApiEndpoint = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${API_KEY}`;
-      const response = await fetch(stockApiEndpoint);
-      const apiData = await response.json();
-      const lastRefreshed = apiData[METADATA_KEY][LATEST_REFRESH_KEY];
-      const timeSeries = apiData[TIME_SERIES_KEY];
-      const latestData = timeSeries[lastRefreshed];
-      lastValue = latestData[CLOSE_VALUE_KEY];
-    } else {
-      const lastRefreshed = jsonData[METADATA_KEY][LATEST_REFRESH_KEY];
-      const timeSeries = jsonData[TIME_SERIES_KEY];
-      const latestData = timeSeries[lastRefreshed];
-      lastValue = latestData[CLOSE_VALUE_KEY];
-    }
-
-    if (ticker === 'V') {
-      lastValue = '280.43';
-    } else if (ticker === 'MC') {
-      lastValue = '471.6';
-    } else if (ticker === 'GOOG') {
-      lastValue = '132.56';
-    } else if (ticker === 'EBAY') {
-      lastValue = '50.54';
-    } else if (ticker === 'MCO') {
-      lastValue = '389.03';
-    }
+  // Make API request to get stock prices
+  // TODO: Once the server is hosted, swap out the endpoint from localhost
+    const stockApiEndpoint = `http://localhost:5000/api?symbol=${ticker}`;
+    const response = await fetch(stockApiEndpoint);
+    const apiData = await response.json();
+    const lastRefreshed = apiData[METADATA_KEY][LATEST_REFRESH_KEY];
+    const timeSeries = apiData[TIME_SERIES_KEY];
+    const latestData = timeSeries[lastRefreshed];
+    const lastValue = latestData[CLOSE_VALUE_KEY];
     console.log(ticker, lastValue);
     const parsedValueToDecimalPoints = parseFloat(parseFloat(lastValue).toFixed(2));
 
